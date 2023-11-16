e346f29d801739cdc5cf270c7d70026b5a086626
Benjamin Kollmer <bk73630@gmail.com>
2023-11-16 21:09:07 +0100

    Create Controller

---
+++ Contactly/Controller/ConfigController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Contactly.Controller
+{
+    public class ConfigController
+    {
+        private const string ConfigFileName = "config.json";
+
+        // Der vollständige Pfad zur Konfigurationsdatei
+        private string ConfigFilePath => Path.Combine(GetSettingsFolderPath(), ConfigFileName);
+
+        // Erstellt die Konfigurationsdatei, wenn sie nicht existiert
+        public void CreateConfigFileIfNotExists()
+        {
+            try
+            {
+                string appDataFolderPath = ApplicationData.Current.LocalFolder.Path;
+                string settingsFolderPath = Path.Combine(appDataFolderPath, "Settings");
+
+                // Überprüfen, ob der "Settings"-Ordner vorhanden ist, andernfalls erstellen
+                if (!Directory.Exists(settingsFolderPath))
+                {
+                    Directory.CreateDirectory(settingsFolderPath);
+                }
+
+                // Überprüfen, ob die Konfigurationsdatei vorhanden ist, andernfalls erstellen
+                if (!File.Exists(ConfigFilePath))
+                {
+                    var defaultConfig = new AppConfig { VcfFolderPath = string.Empty };
+                    string defaultConfigJson = JsonConvert.SerializeObject(defaultConfig);
+                    File.WriteAllText(ConfigFilePath, defaultConfigJson);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Behandeln Sie Ausnahmen, die während des Erstellungsprozesses auftreten können
+                Console.WriteLine($"Fehler beim Erstellen der Konfigurationsdatei: {ex.Message}");
+            }
+        }
+
+        // Überprüft, ob die Konfigurationsdatei vorhanden ist
+        public bool ConfigFileExists()
+        {
+            return File.Exists(ConfigFilePath);
+        }
+
+        // Löscht die Konfigurationsdatei
+        public void DeleteConfigFile()
+        {
+            if (File.Exists(ConfigFilePath))
+            {
+                File.Delete(ConfigFilePath);
+            }
+        }
+
+        // Lädt die Konfigurationsdaten aus der Datei oder gibt einen leeren Wert zurück, wenn die Datei nicht gefunden wurde
+        private async Task<AppConfig> LoadConfigAsync()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(ConfigFilePath))
+                {
+                    string json = await sr.ReadToEndAsync();
+                    return JsonConvert.DeserializeObject<AppConfig>(json);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                return new AppConfig { VcfFolderPath = string.Empty };
+            }
+        }
+
+        // Speichert die Konfigurationsdaten in der Datei
+        private async Task SaveConfigAsync(AppConfig configData)
+        {
+            string json = JsonConvert.SerializeObject(configData);
+
+            using (StreamWriter sw = new StreamWriter(ConfigFilePath))
+            {
+                await sw.WriteAsync(json);
+            }
+        }
+
+        // Gibt den ausgewählten Pfad aus der Konfigurationsdatei zurück
+        public async Task<string> GetSelectedPathAsync()
+        {
+            var configData = await LoadConfigAsync();
+            return configData?.VcfFolderPath;
+        }
+
+        // Speichert den ausgewählten Pfad in der Konfigurationsdatei
+        public async Task SaveSelectedPathAsync(string selectedPath)
+        {
+            var configData = await LoadConfigAsync();
+            configData.VcfFolderPath = selectedPath;
+            await SaveConfigAsync(configData);
+        }
+
+        // Hilfsmethode zur Erstellung des Ordnerpfads für die Einstellungen
+        private string GetSettingsFolderPath()
+        {
+            string appDataFolderPath = ApplicationData.Current.LocalFolder.Path;
+            return Path.Combine(appDataFolderPath, "Settings");
+        }
+    }
+
+    // Die Datenklasse für die Konfiguration
+    public class AppConfig
+    {
+        public string VcfFolderPath { get; set; }
+    }
+}
---
+++ Contactly/Controller/InitController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Contactly.Controller;
+
+namespace Contactly.Controller
+{
+    /// <summary>
+    /// Ein Controller, der die Anwendungsinitialisierung durchführt.
+    /// </summary>
+    public class InitController
+    {
+        private readonly ConfigController _configController;
+
+        /// <summary>
+        /// Konstruktor für den InitController.
+        /// </summary>
+        public InitController()
+        {
+            // Initialisieren Sie den ConfigController.
+            _configController = new ConfigController();
+        }
+
+        /// <summary>
+        /// Diese Methode wird beim Start der Anwendung aufgerufen, um die Initialisierung durchzuführen.
+        /// </summary>
+        /// <returns>Ein Task, der die Initialisierung durchführt.</returns>
+        public async Task InitializeAsync()
+        {
+            // Überprüfen Sie, ob die Config-Datei existiert, und erstellen Sie sie, wenn sie nicht vorhanden ist.
+            _configController.CreateConfigFileIfNotExists();
+        }
+    }
+}
